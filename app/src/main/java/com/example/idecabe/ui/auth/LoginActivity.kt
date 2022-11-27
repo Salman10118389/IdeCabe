2952f97cc50d41e9732883f580570233719364d7
vaniamzf <vaniamzf@gmail.com>
2022-11-27 15:16:28 +0700

    activity login

--- app/src/main/java/com/example/idecabe/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/idecabe/ui/auth/LoginActivity.kt
@@ -13,6 +13,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
